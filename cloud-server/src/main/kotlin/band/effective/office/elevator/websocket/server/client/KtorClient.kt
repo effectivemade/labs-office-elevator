59dcc6eee5b17a26950e521ea79aeee251a8a11f
Stanislav Radchenko <stas.radchenko@effective.band>
2023-01-12 20:53:47 +0600

    - Rename projects

---
+++ cloud-server/src/main/kotlin/band/effective/office/elevator/websocket/server/client/KtorClient.kt
@@ -0,0 +1,12 @@
+package band.effective.office.elevator.websocket.server.client
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+
+val ktorClient = HttpClient(CIO) {
+    defaultRequest {
+        host = "92.124.138.130"
+        port = 2023
+    }
+}
