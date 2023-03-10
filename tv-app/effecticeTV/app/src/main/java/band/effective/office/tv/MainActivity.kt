3ccf41947985b7b1c0a263c90aedca506ab26f1a
Maksim Mishenko <kirova32a@mail.ru>
2023-03-10 17:57:13 +0600

    [~] fix

---
+++ tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/MainActivity.kt
@@ -0,0 +1,50 @@
+package band.effective.office.tv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.tv.foundation.lazy.list.TvLazyRow
+import com.example.effecticetv.ui.theme.EffecticeTVTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EffecticeTVTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting(name = "Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    TvLazyRow (
+        contentPadding = PaddingValues(16.dp),
+        userScrollEnabled = true
+        ){
+        items(100) {
+            Image(
+                modifier = Modifier.clickable {  },
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = ""
+            )
+            Spacer(modifier = Modifier.width(32.dp))
+        }
+    }
+}
+
