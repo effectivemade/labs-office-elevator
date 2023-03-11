6cde0a8c22e380fcc29fee2d9bebc28a909a6640
Maksim Mishenko <kirova32a@mail.ru>
2023-03-11 23:59:02 +0600

    [+] add a coil to check the path to the photo

--- tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/view/ListView.kt
+++ tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/view/ListView.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.tv.material3.Text
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun ListView(){
@@ -17,6 +20,13 @@ fun ListView(){
                 val item = response!!.data.items[i]
                 Text(item.fullName)
                 Text(item.format)
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(item.photo)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = item.fullName
+                )
             }
         }
     }
