8b1dbb917ac400864cd033682ff868c30b7dc63b
Maksim Mishenko <kirova32a@mail.ru>
2023-03-28 18:05:49 +0600

    [+] copy LoadScreen from LeaderApi

--- tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/ui/theme/Type.kt
+++ tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.example.effecticetv.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import band.effective.office.tv.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -26,3 +29,12 @@ val Typography = Typography(
     )
     */
 )
+
+fun robotoFontFamily(): FontFamily =
+    FontFamily(
+        Font(R.font.roboto_black),
+        Font(R.font.roboto_black, weight = FontWeight.Bold),
+        Font(R.font.roboto_black, weight = FontWeight.Light),
+        Font(R.font.roboto_black, weight = FontWeight.Thin),
+        Font(R.font.roboto_black, weight = FontWeight.Normal, style = FontStyle.Italic)
+    )
