183b144f1f32ef96210a45b129656b75f4b11cce
Artem Gruzdev <myposhta.ruzdev@gmail.com>
2023-03-30 15:37:33 +0600

    [~] refactor code

--- tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/screens/photo/PhotoViewModel.kt
+++ tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/screens/photo/PhotoViewModel.kt
@@ -33,21 +33,10 @@ class PhotoViewModel @Inject constructor(
             callbackToEnd = { mutableEffect.emit(BestPhotoEffect.ScrollToNextItem) },
             isPlay = true
         )
-        slideShow.startTimer()
 
-//        viewModelScope.launch {
-//            repository.auth()
-//            updatePhoto()
-//        }
-
-        mutableState.update {
-            it.copy(
-                photos = listOf(
-                    Photo("https://media.istockphoto.com/id/1346125184/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D0%B0-%D1%83%D1%81%D0%BF%D0%B5%D1%88%D0%BD%D0%BE%D0%B9-%D0%BC%D0%BD%D0%BE%D0%B3%D0%BE%D0%BD%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9-%D0%B1%D0%B8%D0%B7%D0%BD%D0%B5%D1%81-%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D1%8B.jpg?s=612x612&w=is&k=20&c=nbkK2hQxub07nehsvMxamrImCg_ptggazCFX8aC0nMg="),
-                    Photo("https://media.istockphoto.com/id/635975374/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BC%D1%8B-%D0%B2%D1%81%D0%B5%D0%B3%D0%B4%D0%B0-%D0%B4%D0%BE%D1%81%D1%82%D0%B8%D0%B3%D0%B0%D0%B5%D0%BC-%D0%BE%D0%BF%D1%82%D0%B8%D0%BC%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85-%D1%80%D0%B5%D0%B7%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%82%D0%BE%D0%B2-%D0%B2%D0%BC%D0%B5%D1%81%D1%82%D0%B5.jpg?s=612x612&w=is&k=20&c=47v_bNZIIvmlasx3UA8EqCInKt7fzwsL6iAFu7vPaUI="),
-                    Photo("https://media.istockphoto.com/id/968943374/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%81%D1%82%D1%80%D0%B5%D0%BC%D0%B8%D1%82%D0%B5%D1%81%D1%8C-%D0%BA-%D1%83%D1%81%D0%BF%D0%B5%D1%85%D1%83-%D0%B8-%D0%B2%D1%8B-%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D0%B0-%D0%B1%D1%83%D0%B4%D0%B5%D1%82-%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D1%8D%D1%82%D0%BE%D0%BC%D1%83-%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%83.jpg?s=612x612&w=is&k=20&c=j_FxgYm9EDhwnfPuwXi-02ROOa4xyM-PjwOluGnqiQU="),
-                )
-            )
+        viewModelScope.launch {
+            repository.auth()
+            updatePhoto()
         }
     }
 
@@ -56,14 +45,12 @@ class PhotoViewModel @Inject constructor(
             is BestPhotoEvent.OnClickNextItem -> {
                 slideShow.resetTimer()
                 viewModelScope.launch {
-                    Log.d("INDEX", "index = ${event.index}")
                     mutableEffect.emit(BestPhotoEffect.ScrollToItem(event.index + 1))
                 }
             }
             is BestPhotoEvent.OnClickPreviousItem -> {
                 slideShow.resetTimer()
                 viewModelScope.launch {
-                    Log.d("INDEX", "index = ${event.index}")
                     mutableEffect.emit(BestPhotoEffect.ScrollToItem(event.index - 1))
                 }
             }
@@ -77,7 +64,6 @@ class PhotoViewModel @Inject constructor(
                     slideShow.startTimer()
                 }
                 viewModelScope.launch {
-                    Log.d("INDEX", "index = $isPlay")
                     mutableEffect.emit(BestPhotoEffect.ChangePlayState(isPlay))
                 }
             }
@@ -91,28 +77,14 @@ class PhotoViewModel @Inject constructor(
                     mutableState.update { state ->
                         state.copy(isSuccess = false, error = result.error)
                     }
-                    Log.e("TAG", result.error)
                 }
                 is Resource.Data -> {
-                    Log.d("href", result.data.toString())
                     mutableState.update { state ->
                         state.copy(photos = result.toUIModel(), isSuccess = true)
                     }
+                    slideShow.startTimer()
                 }
             }
         }
     }
-//    private fun startTimer() {
-//        timerJob?.cancel()
-//        timerJob = viewModelScope.launch(Dispatchers.Default) {
-//            while (isActive && mutableState.value.isPlay){
-//                val startTime = System.currentTimeMillis()
-//                var currentTime = System.currentTimeMillis()
-//                while (isActive && currentTime - startTime < BuildConfig.slideShowPeriod * 1000)
-//                    currentTime = System.currentTimeMillis()
-//                if (isActive)
-//                    mutableEffect.emit(BestPhotoEffect.ScrollToNextItem)
-//            }
-//        }
-//    }
 }
