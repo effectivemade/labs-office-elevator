e086f7e491f7ee3fe7c2f064ef1f7540afb3d3c3
Maksim Mishenko <kirova32a@mail.ru>
2023-04-02 22:00:27 +0600

    [~] fix

--- tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/screen/menu/component/MenuItems.kt
+++ tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/screen/menu/component/MenuItems.kt
@@ -1,5 +1,6 @@
 package band.effective.office.tv.screen.menu.component
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -9,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.sp
 import band.effective.office.tv.ui.theme.QuickSilver
 import band.effective.office.tv.ui.theme.VividTangelo
@@ -18,12 +20,24 @@ import com.example.effecticetv.ui.theme.robotoFontFamily
 fun MenuItem(
     text: String, modifier: Modifier = Modifier, onClick: () -> Unit, onFocus: (Boolean) -> Unit
 ) {
-    var isSelect by remember { mutableStateOf(false) }
+    var isFocus by remember { mutableStateOf(false) }
+    val animatedBackgroundColor by animateColorAsState(
+        targetValue =
+        if (isFocus) VividTangelo
+        else QuickSilver)
     Box(modifier = modifier
-        .background(if (isSelect) VividTangelo else QuickSilver)
-        .fillMaxHeight(if (isSelect) 0.95f else 0.9f)
+        .graphicsLayer {
+            scaleX =
+                if (isFocus) 1.1f
+                else 1f
+            scaleY =
+                if (isFocus) 1.1f
+                else 1f
+        }
+        .background(animatedBackgroundColor)
+        .fillMaxHeight(if (isFocus) 0.95f else 0.9f)
         .onFocusChanged {
-            isSelect = it.isFocused
+            isFocus = it.isFocused
             onFocus(it.isFocused)
         }
         .clickable { onClick() }, contentAlignment = Alignment.Center
