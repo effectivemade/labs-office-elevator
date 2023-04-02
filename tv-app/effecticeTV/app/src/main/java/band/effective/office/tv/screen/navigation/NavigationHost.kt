e931a54da48ff00ad7ef460619c2599ede078870
Maksim Mishenko <kirova32a@mail.ru>
2023-04-02 21:03:30 +0600

    [~] refactor navigation

--- tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/screen/navigation/NavigationHost.kt
+++ tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/screen/navigation/NavigationHost.kt
@@ -10,31 +10,15 @@ import band.effective.office.tv.screen.menu.MenuScreen
 @Composable
 fun NavigationHost() {
     val navController = rememberNavController()
-    //TODO(Artem Gruzdev): replace title screen in string res
-    val screensList = listOf(
-        NavigationModel(
-            title = "Events",
-            screen = Screen.Events
-        ),
-        NavigationModel(
-            title = "Best Photo",
-            screen = Screen.BestPhoto),
-        NavigationModel(
-            screen = Screen.History,
-            title = "History"
-        )
-    )
-    NavHost(navController = navController, startDestination = Screen.Main.name) {
-        // TODO(Maksim Mishenko): replace text with desired screen
-        composable(Screen.Main.name) {
-
+    NavHost(navController = navController, startDestination = "menu") {
+        composable("menu") {
             MenuScreen(
-                itemsList = screensList,
+                itemsList = NavigationModel.screensList,
                 navController = navController
             )
         }
-        composable(Screen.Events.name) { Text("Events") }
-        composable(Screen.BestPhoto.name) { Text("Best Photo") }
-        composable(Screen.History.name) { Text("History") }
+        for (screen in NavigationModel.screensList){
+            composable(screen.title) {screen.screen()}
+        }
     }
 }
--- tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/screen/navigation/NavigationModel.kt
+++ tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/screen/navigation/NavigationModel.kt
@@ -1,13 +1,33 @@
 package band.effective.office.tv.screen.navigation
 
-enum class Screen {
-    Main,
-    BestPhoto,
-    Events,
-    History
-}
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+// TODO(Maksim Mishenko): paste screens
 
 data class NavigationModel(
-    val screen: Screen,
-    val title: String
-)
+    val screen: @Composable () -> Unit, val title: String
+) {
+    companion object {
+        val screensList = listOf(
+            NavigationModel(@Composable { LeaderIdEventScreen() }, "Events"),
+            NavigationModel(@Composable { BestFotoScreen() }, "Best Photo"),
+            NavigationModel(@Composable { HistoryScreen() }, "History")
+        )
+    }
+}
+
+@Composable
+fun LeaderIdEventScreen() {
+    Text(text = "Events")
+}
+
+@Composable
+fun BestFotoScreen() {
+    Text("Best Photo")
+}
+
+@Composable
+fun HistoryScreen() {
+    Text("History")
+}
