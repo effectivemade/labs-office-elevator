60e0fd96e604fd1e77f8dce75c50084bc2c9f061
Artem Gruzdev <myposhta.ruzdev@gmail.com>
2023-04-30 12:18:55 +0600

    [~] fix after merge

--- tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/screen/photo/PhotoViewModel.kt
+++ tv-app/effecticeTV/app/src/main/java/band/effective/office/tv/screen/photo/PhotoViewModel.kt
@@ -2,9 +2,9 @@ package band.effective.office.tv.screen.photo
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import band.effective.office.tv.BuildConfig
 import band.effective.office.tv.core.ui.screen_with_controls.TimerSlideShow
 import band.effective.office.tv.core.network.entity.Either
+import band.effective.office.tv.domain.autoplay.AutoplayableViewModel
 import band.effective.office.tv.repository.synology.SynologyRepository
 import band.effective.office.tv.screen.photo.model.toUIModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -75,6 +75,7 @@ class PhotoViewModel @Inject constructor(
                     mutableEffect.emit(BestPhotoEffect.ChangePlayState(isPlay))
                 }
             }
+            else -> {}
         }
     }
 
