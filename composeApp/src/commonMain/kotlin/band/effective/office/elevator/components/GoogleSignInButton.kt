278a90153b07cfb25b84928f9c9018cbab31601b
Slava <plumsoftwareofficial@gmail.com>
2023-07-04 16:23:37 +0600

    Changes with Google sign in button: added stroke and shape, content changes with padding

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/components/GoogleSignInButton.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/components/GoogleSignInButton.kt
@@ -1,6 +1,7 @@
 package band.effective.office.elevator.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import band.effective.office.elevator.MainRes
 import io.github.skeptick.libres.compose.painterResource
@@ -21,11 +23,12 @@ import io.github.skeptick.libres.compose.painterResource
 internal fun GoogleSignInButton(modifier: Modifier, onClick: () -> Unit) {
     Button(
         onClick = onClick,
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(6.dp),
+        modifier = modifier.fillMaxWidth()
+            .border(2.dp, MaterialTheme.colors.secondary, RoundedCornerShape(40.dp)),
+        shape = RoundedCornerShape(40.dp),
         colors = ButtonDefaults.buttonColors(
-            backgroundColor = MaterialTheme.colors.surface,
-            contentColor = MaterialTheme.colors.onSurface
+            backgroundColor = Color.Transparent,
+            contentColor = Color(0xFFC2410C)
         )
     ) {
         Image(
@@ -33,7 +36,7 @@ internal fun GoogleSignInButton(modifier: Modifier, onClick: () -> Unit) {
             contentDescription = null,
             modifier = Modifier.size(32.dp)
         )
-        Spacer(modifier = Modifier.width(8.dp))
+        Spacer(modifier = Modifier.width(12.dp))
         Text(text = MainRes.string.sign_in_google, modifier = Modifier.padding(6.dp))
     }
 }
