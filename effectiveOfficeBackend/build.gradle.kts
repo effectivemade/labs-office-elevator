d18e9f3a6e2cc6668e0716211d5da434b17ffc43
Kiselev Danil <dabikis2855@gmail.com>
2023-07-04 14:38:43 +0600

    [+] server request validation dependency

--- effectiveOfficeBackend/build.gradle.kts
+++ effectiveOfficeBackend/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation ("io.insert-koin:koin-ktor:$koin_version")
     implementation ("io.insert-koin:koin-logger-slf4j:$koin_version")
+    implementation("io.ktor:ktor-server-request-validation:$ktor_version")
 
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
