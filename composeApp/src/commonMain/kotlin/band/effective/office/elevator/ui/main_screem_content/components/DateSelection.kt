01511b5766368eee3ec04aaf731ddfa8f7fac0b8
Artem Gruzdev <myposhta.ruzdev@gmail.com>
2023-07-04 12:46:47 +0600

    [~] save commit

---
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/main_screem_content/components/DateSelection.kt
@@ -0,0 +1,31 @@
+package band.effective.office.elevator.ui.main_screem_content.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DateSelection() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround
+    ){
+        Text(
+            text = "",
+            fontSize = 15.sp,
+        )
+
+    }
+}
+
+@Composable
+fun CalendarTitle() {
+    Row {
+        
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/main_screem_content/components/SeatReservation.kt
@@ -0,0 +1,9 @@
+package band.effective.office.elevator.ui.main_screem_content.components
+
+import androidx.compose.runtime.Composable
+import band.effective.office.elevator.ui.models.ReservedSeat
+
+@Composable
+fun SeatReservation(reservedSeats: List<ReservedSeat>) {
+
+}
