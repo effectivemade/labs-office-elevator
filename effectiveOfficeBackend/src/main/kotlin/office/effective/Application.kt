713717725a24da1a62cb71e1b938963edc790714
Kiselev Danil <dabikis2855@gmail.com>
2023-07-06 00:16:16 +0600

    [~] changed port and host declaration. Now it takes from application.conf

--- effectiveOfficeBackend/src/main/kotlin/office/effective/Application.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/Application.kt
@@ -1,13 +1,21 @@
 package office.effective
 
+import com.typesafe.config.ConfigFactory
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import office.effective.plugins.*
 
+
+val config = HoconApplicationConfig(ConfigFactory.load())
+
+val portNumber: Int = config.propertyOrNull("ktor.deployment.port")?.getString()?.toIntOrNull() ?: 8080
+val hostId: String = config.propertyOrNull("ktor.deployment.host")?.getString() ?: "0.0.0.0"
 fun main() {
-    embeddedServer(factory = Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(factory = Netty, port = portNumber, host = hostId, module = Application::module)
         .start(wait = true)
+
 }
 
 fun Application.module() {
