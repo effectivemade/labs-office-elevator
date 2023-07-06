eb750f025afbb5ceeff2096764a75cd0fc113990
Maksim Mishenko <kirova32a@mail.ru>
2023-07-06 20:50:51 +0600

    [~] add switch on FreeNegotiationsScreen

--- tabletApp/src/commonMain/kotlin/band/effective/office/tablet/ui/root/RootComponent.kt
+++ tabletApp/src/commonMain/kotlin/band/effective/office/tablet/ui/root/RootComponent.kt
@@ -1,5 +1,6 @@
 package band.effective.office.tablet.ui.root
 
+import band.effective.office.tablet.ui.freeNegotiationsScreen.FreeNegotiationsComponent
 import band.effective.office.tablet.ui.mainScreen.RealMainComponent
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.StackNavigation
@@ -35,12 +36,12 @@ class RootComponent(componentContext: ComponentContext) : ComponentContext by co
         }
 
         is Config.SelectRoom -> {
-            Child.SelectRoomChild(SelectRoomComponent(componentContext))
+            Child.SelectRoomChild(FreeNegotiationsComponent(componentContext))
         }
     }
 
     sealed class Child {
-        data class SelectRoomChild(val component: SelectRoomComponent) : Child()
+        data class SelectRoomChild(val component: FreeNegotiationsComponent) : Child()
         data class MainChild(val component: RealMainComponent) : Child()
     }
 
