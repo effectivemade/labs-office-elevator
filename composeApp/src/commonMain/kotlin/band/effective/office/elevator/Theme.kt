a1f94efc134ff1ade016a839a49adeca2a9ed747
Slava <plumsoftwareofficial@gmail.com>
2023-07-10 11:14:01 +0600

    Take out of theme typography initialization

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/Theme.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/Theme.kt
@@ -46,6 +46,86 @@ internal fun AppTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
+    val typography = Typography(
+        defaultFontFamily = getDefaultFont(),
+
+//          region:: H
+        h4 = TextStyle(
+            fontWeight = FontWeight(400),
+            fontSize = 40.sp,
+            fontFamily = getPromoFont(),
+            letterSpacing = 0.1.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Start
+        ),
+
+        h5 = TextStyle(
+            fontWeight = FontWeight(600),
+            fontSize = 28.sp,
+            letterSpacing = 0.1.sp,
+            textAlign = TextAlign.Start,
+            color = Color(0xFF5800CB),
+            lineHeight = 36.4.sp
+        ),
+
+        h6 = TextStyle(
+            fontSize = 20.sp,
+            lineHeight = 26.sp,
+            fontWeight = FontWeight(600),
+            textAlign = TextAlign.Center,
+            letterSpacing = 0.1.sp,
+        ),
+
+//          endregion
+
+//          region::Subtitles
+        subtitle1 = TextStyle(
+            fontSize = 15.sp,
+            lineHeight = 19.5.sp,
+            letterSpacing = 0.1.sp,
+//          Setup font weight and color
+        ),
+//          endregion
+
+//          region::Body styles
+        body1 = TextStyle(
+            fontWeight = FontWeight(500),
+            fontSize = 16.sp,
+            lineHeight = 20.8.sp,
+            letterSpacing = 0.1.sp,
+            textAlign = TextAlign.Start,
+            color = theme_light_tertiary_color
+        ),
+
+        body2 = TextStyle(
+            fontSize = 14.sp,
+            lineHeight = 18.2.sp,
+            fontWeight = FontWeight(500),
+            color = theme_light_secondary_color,
+            letterSpacing = 0.1.sp,
+        ),
+
+        caption = TextStyle(
+            fontSize = 12.sp,
+            lineHeight = 15.6.sp,
+            fontWeight = FontWeight(500),
+            color = theme_light_tertiary_color,
+            textAlign = TextAlign.Center,
+            letterSpacing = 0.1.sp,
+        ),
+//          endregion
+
+//          region::Button styles
+        button = TextStyle(
+            fontWeight = FontWeight(500),
+            fontSize = 16.sp,
+            letterSpacing = 0.1.sp,
+            lineHeight = 20.8.sp,
+            textAlign = TextAlign.Center
+        ),
+//          endregion
+
+    )
     val colors = if (!useDarkTheme) {
         LightColors
     } else {
@@ -53,86 +133,7 @@ internal fun AppTheme(
     }
 
     MaterialTheme(
-        typography = Typography(
-            defaultFontFamily = getDefaultFont(),
-
-//          region:: H
-            h4 = TextStyle(
-                fontWeight = FontWeight(400),
-                fontSize = 40.sp,
-                fontFamily = getPromoFont(),
-                letterSpacing = 0.1.sp,
-                lineHeight = 40.sp,
-                textAlign = TextAlign.Start
-            ),
-
-            h5 = TextStyle(
-                fontWeight = FontWeight(600),
-                fontSize = 28.sp,
-                letterSpacing = 0.1.sp,
-                textAlign = TextAlign.Start,
-                color = Color(0xFF5800CB),
-                lineHeight = 36.4.sp
-            ),
-
-            h6 = TextStyle(
-                fontSize = 20.sp,
-                lineHeight = 26.sp,
-                fontWeight = FontWeight(600),
-                textAlign = TextAlign.Center,
-                letterSpacing = 0.1.sp,
-            ),
-
-//          endregion
-
-//          region::Subtitles
-            subtitle1 = TextStyle(
-                fontSize = 15.sp,
-                lineHeight = 19.5.sp,
-                letterSpacing = 0.1.sp,
-//          Setup font weight and color
-            ),
-//          endregion
-
-//          region::Body styles
-            body1 = TextStyle(
-                fontWeight = FontWeight(500),
-                fontSize = 16.sp,
-                lineHeight = 20.8.sp,
-                letterSpacing = 0.1.sp,
-                textAlign = TextAlign.Start,
-                color = theme_light_tertiary_color
-            ),
-
-            body2 = TextStyle(
-                fontSize = 14.sp,
-                lineHeight = 18.2.sp,
-                fontWeight = FontWeight(500),
-                color = theme_light_secondary_color,
-                letterSpacing = 0.1.sp,
-            ),
-
-            caption = TextStyle(
-                fontSize = 12.sp,
-                lineHeight = 15.6.sp,
-                fontWeight = FontWeight(500),
-                color = theme_light_tertiary_color,
-                textAlign = TextAlign.Center,
-                letterSpacing = 0.1.sp,
-            ),
-//          endregion
-
-//          region::Button styles
-            button = TextStyle(
-                fontWeight = FontWeight(500),
-                fontSize = 16.sp,
-                letterSpacing = 0.1.sp,
-                lineHeight = 20.8.sp,
-                textAlign = TextAlign.Center
-            ),
-//          endregion
-
-        ),
+        typography = typography,
         colors = colors,
         content = {
             Surface(content = content)
