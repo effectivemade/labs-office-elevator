b8c1dae9cb64cc2ff53c1b5a521e969a70cf9c03
Kiselev Danil <dabikis2855@gmail.com>
2023-07-11 13:46:39 +0600

    [+] added fat jar settings

--- effectiveOfficeBackend/build.gradle.kts
+++ effectiveOfficeBackend/build.gradle.kts
@@ -18,6 +18,12 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+ktor {
+    fatJar {
+        archiveFileName.set("backendApp.jar")
+    }
+}
+
 repositories {
     mavenCentral()
 }
