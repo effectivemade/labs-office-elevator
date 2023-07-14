fb7037a0d5bf955ae2e9802c822cb46e0284a647
Kiselev Danil <dabikis2855@gmail.com>
2023-07-14 10:04:46 +0600

    [+] added Sessions (headers) and oauth installations

--- effectiveOfficeBackend/src/main/kotlin/office/effective/Application.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/Application.kt
@@ -1,10 +1,19 @@
 package office.effective
 
 import com.typesafe.config.ConfigFactory
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.engine.cio.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
 import office.effective.common.utils.UserSession
 import office.effective.plugins.*
@@ -25,6 +34,24 @@ fun Application.module() {
     configureSecurity()
     configureRouting()
     install(Sessions) {
-        cookie<UserSession>("user_session")
+        // TODO: Cookies
+        header<UserSession>("user_session")
+    }
+    authentication {
+        oauth("auth-oauth-google") {
+            urlProvider = { "http://localhost:8080/callback" }
+            providerLookup = {
+                OAuthServerSettings.OAuth2ServerSettings(
+                    name = "google",
+                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
+                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
+                    requestMethod = HttpMethod.Post,
+                    clientId = "27867488691-gc95ln5jad3i84dcmu3dd9ls1s4hvm9c.apps.googleusercontent.com", /*System.getenv("GOOGLE_CLIENT_ID")*/
+                    clientSecret = "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",//System.getenv("GOOGLE_CLIENT_SECRET"),
+                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile")
+                )
+            }
+            client = HttpClient(Apache)
+        }
     }
 }
