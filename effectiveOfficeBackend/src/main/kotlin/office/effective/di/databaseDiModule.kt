3d9c4f3621983ae0af4115c6950e878196c9fa50
Kiselev Danil <dabikis2855@gmail.com>
2023-07-14 14:51:59 +0600

    [~] added a flag "createAtStart" to Database bean

--- effectiveOfficeBackend/src/main/kotlin/office/effective/di/databaseDiModule.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/di/databaseDiModule.kt
@@ -1,12 +1,11 @@
 package office.effective.di
 
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.ktorm.database.Database
 
 val databaseDiModule = module {
     //info about db connection.
-    single<Database> {
+    single<Database>(createdAtStart = true) {
         Database.connect(
             url = "jdbc:postgresql://localhost:15432/effectiveOfficeBackendDB",
             driver = "org.postgresql.Driver",
