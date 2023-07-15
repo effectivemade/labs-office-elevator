40f64baf441421b134bba2541170ae2bd14fb469
Slava <plumsoftwareofficial@gmail.com>
2023-07-15 14:01:49 +0600

    Added AuthTabItem

---
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/authorization/components/AuthTabItem.kt
@@ -0,0 +1,41 @@
+package band.effective.office.elevator.ui.authorization.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.Tab
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import band.effective.office.elevator.theme_light_secondary_icon_color
+import band.effective.office.elevator.theme_light_tertiary_icon_color
+
+@Composable
+fun AuthTabItem(selected: Boolean) {
+
+    val selectedColor = remember { mutableStateOf(theme_light_secondary_icon_color) }
+    val unselectedColor = remember { mutableStateOf(theme_light_tertiary_icon_color) }
+
+    Tab(
+        selectedContentColor = selectedColor.value,
+        unselectedContentColor = unselectedColor.value,
+        selected = selected,
+        onClick = { },
+        enabled = false,
+        modifier = Modifier
+            .height(8.dp),
+        icon = {
+            Box(modifier = Modifier.padding(horizontal = 8.dp).clip(RoundedCornerShape(8.dp))) {
+                Divider(
+                    modifier = Modifier.height(8.dp),
+                    color = if (selected) selectedColor.value else unselectedColor.value
+                )
+            }
+        }
+    )
+}
