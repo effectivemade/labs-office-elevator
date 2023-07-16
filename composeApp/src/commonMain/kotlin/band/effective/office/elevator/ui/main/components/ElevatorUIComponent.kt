16076a100dc71d5c069aeefaca8f170d2ce23d11
Leci <leca.deca@mail.ru>
2023-07-16 16:37:52 +0600

    Added some header styles from the theme in the screen "Main"

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/main/components/ElevatorUIComponent.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/main/components/ElevatorUIComponent.kt
@@ -2,11 +2,13 @@ package band.effective.office.elevator.ui.main.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 import band.effective.office.elevator.MainRes
 import band.effective.office.elevator.components.ElevatorUpButton
@@ -19,8 +21,8 @@ fun ElevatorUIComponent(
     Row(verticalAlignment =Alignment.CenterVertically) {
         Text(
             text = stringResource(MainRes.strings.elevator),
-            fontSize = 15.sp, // TODO использовать тему
-            color = Color.Black
+            style = MaterialTheme.typography.subtitle1,
+            color = Color.Black //this color doesn't in Theme
         )
         Spacer(Modifier.weight(.1f))
         ActionCall(onClickCallElevator)
