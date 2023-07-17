a8ef8f34eb78093e30e160df92cfcf2349528b0c
Kiselev Danil <dabikis2855@gmail.com>
2023-07-17 15:37:55 +0600

    [~] reworked TokenVerifier. Now check domain and returns email

--- effectiveOfficeBackend/src/main/kotlin/office/effective/feature/auth/TokenVerifier.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/feature/auth/TokenVerifier.kt
@@ -1,22 +1,41 @@
-package office.effective.common.utils
+package office.effective.feature.auth
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.gson.GsonFactory
 
-object TokenVerifier {
+class TokenVerifier : ITokenVerifier {
 
-    private const val webClient =
+    private val webClient =
         "27867488691-gc95ln5jad3i84dcmu3dd9ls1s4hvm9c.apps.googleusercontent.com"
-
-    private const val verifyDomain = "effective.band"
 
     private val verifier: GoogleIdTokenVerifier =
         GoogleIdTokenVerifier.Builder(NetHttpTransport(), GsonFactory()).setAudience(listOf(webClient)).build()
 
-    fun isCorrectToken(tokenString: String): GoogleIdToken? {
-        return verifier.verify(tokenString)
+    private val acceptableMailDomain: String = "effective.band"
+
+    override fun isCorrectToken(tokenString: String): String {
+        var userMail: String? = null
+        val token: GoogleIdToken? = verifier.verify(tokenString)
+
+        if (token != null) {
+            val payload: GoogleIdToken.Payload = token.payload
+
+            val emailVerified: Boolean = payload.emailVerified
+            val hostedDomain = payload.hostedDomain
+
+            if ((acceptableMailDomain == hostedDomain) && emailVerified) {
+                userMail = payload.email
+            }
+
+        } else {
+            throw Exception("Token cannot be verified")
+        }
+        if (userMail.isNullOrBlank()) {
+            throw Exception("Token cannot be verified")
+        }
+        return userMail
     }
 
 }
