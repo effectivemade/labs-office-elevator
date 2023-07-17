ffed9edcd54ebba86bad6c3c4382915c856838e2
Slava <plumsoftwareofficial@gmail.com>
2023-07-17 11:19:27 +0600

    Code refactored: - added Authorization component with children

---
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/authorization/Authorization.kt
@@ -0,0 +1,23 @@
+package band.effective.office.elevator.ui.authorization
+
+import androidx.compose.runtime.Composable
+import band.effective.office.elevator.ui.authorization.authorization_google.AuthorizationGoogleScreen
+import band.effective.office.elevator.ui.authorization.authorization_phone.AuthorizationPhoneScreen
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.Children
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.plus
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.scale
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
+
+@Composable
+fun AuthorizationScreen(component: AuthorizationComponent) {
+    Children(
+        stack = component.childStack,
+        animation = stackAnimation(fade() + scale()),
+    ) {
+        when (val child = it.instance) {
+            is AuthorizationComponent.Child.GoogleAuthChild -> AuthorizationGoogleScreen(child.component)
+            is AuthorizationComponent.Child.PhoneAuthChild -> AuthorizationPhoneScreen(child.component)
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/authorization/AuthorizationComponent.kt
@@ -0,0 +1,85 @@
+package band.effective.office.elevator.ui.authorization
+
+import band.effective.office.elevator.ui.authorization.authorization_google.AuthorizationGoogleComponent
+import band.effective.office.elevator.ui.authorization.authorization_phone.AuthorizationPhoneComponent
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.router.stack.replaceAll
+import com.arkivanov.decompose.value.Value
+import com.arkivanov.essenty.parcelable.Parcelable
+import com.arkivanov.essenty.parcelable.Parcelize
+import com.arkivanov.mvikotlin.core.store.StoreFactory
+
+class AuthorizationComponent(
+    componentContext: ComponentContext,
+    private val storeFactory: StoreFactory,
+    private val openAuthorizationFlow: () -> Unit
+) :
+    ComponentContext by componentContext {
+
+    private val navigation = StackNavigation<AuthorizationComponent.Config>()
+
+    private val stack = childStack(
+        source = navigation,
+        initialStack = { listOf(AuthorizationComponent.Config.GoogleAuth) },
+        childFactory = ::child,
+        handleBackButton = true
+    )
+
+    val childStack: Value<ChildStack<*, AuthorizationComponent.Child>> = stack
+
+    private fun child(
+        config: AuthorizationComponent.Config,
+        componentContext: ComponentContext
+    ): AuthorizationComponent.Child =
+        when (config) {
+            is AuthorizationComponent.Config.GoogleAuth -> AuthorizationComponent.Child.GoogleAuthChild(
+                AuthorizationGoogleComponent(
+                    componentContext,
+                    storeFactory,
+                    ::googleAuthOutput
+                )
+            )
+
+            is AuthorizationComponent.Config.PhoneAuth -> AuthorizationComponent.Child.PhoneAuthChild(
+                AuthorizationPhoneComponent(
+                    componentContext,
+                    storeFactory,
+                    ::phoneAuthOutput
+                )
+            )
+        }
+
+//    region::Output
+    private fun googleAuthOutput(output: AuthorizationGoogleComponent.Output) {
+        when (output) {
+            AuthorizationGoogleComponent.Output.OpenAuthorizationPhoneScreen -> navigation.replaceAll(
+                Config.PhoneAuth
+            )
+
+            else -> {}
+        }
+    }
+
+    private fun phoneAuthOutput(output: AuthorizationPhoneComponent.Output) {
+        when (output){
+            AuthorizationPhoneComponent.Output.OpenProfileScreen -> TODO()
+        }
+    }
+//    endregion
+
+    sealed class Child {
+        class GoogleAuthChild(val component: AuthorizationGoogleComponent) : Child()
+        class PhoneAuthChild(val component: AuthorizationPhoneComponent) : Child()
+    }
+
+    sealed class Config : Parcelable {
+        @Parcelize
+        object GoogleAuth : Config()
+
+        @Parcelize
+        object PhoneAuth : Config()
+    }
+}
