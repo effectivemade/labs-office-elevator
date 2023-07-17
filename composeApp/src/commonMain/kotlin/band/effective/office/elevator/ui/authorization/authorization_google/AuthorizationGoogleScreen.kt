e4dd961b5db5e48e984e15d1b3f25e12c3d4b154
Slava <plumsoftwareofficial@gmail.com>
2023-07-17 23:40:26 +0600

    Created UI in google sign in page

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/authorization/authorization_google/AuthorizationGoogleScreen.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/authorization/authorization_google/AuthorizationGoogleScreen.kt
@@ -1,16 +1,33 @@
 package band.effective.office.elevator.ui.authorization.authorization_google
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import band.effective.office.elevator.MainRes
 import band.effective.office.elevator.components.GoogleSignInButton
 import band.effective.office.elevator.expects.showToast
 import band.effective.office.elevator.ui.authorization.authorization_google.store.AuthorizationGoogleStore
+import dev.icerock.moko.resources.compose.painterResource
+import dev.icerock.moko.resources.compose.stringResource
 
 
 @Composable
@@ -34,9 +51,36 @@ fun AuthorizationGoogleScreen(component: AuthorizationGoogleComponent) {
 @Composable
 private fun AuthorizationGoogleScreenContent(onEvent: (AuthorizationGoogleStore.Intent) -> Unit) {
     Box(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        GoogleSignInButton(
-            modifier = Modifier.align(Alignment.Center),
-            onClick = { onEvent(AuthorizationGoogleStore.Intent.SignInButtonClicked) })
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painterResource(MainRes.images.effective_logo),
+                    contentDescription = "Effective logo",
+                    modifier = Modifier.size(80.dp)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = stringResource(MainRes.strings.company_name),
+                    letterSpacing = 0.01.em,
+                    fontSize = 40.sp,
+                    color = Color(0xFF323E48), // TODO: Should add this color to Color.kt
+                    style = MaterialTheme.typography.h4
+                )
+            }
+
+            GoogleSignInButton(
+                modifier = Modifier,//.align(alignment = Alignment.Center), MARK: removed align
+                onClick = { onEvent(AuthorizationStore.Intent.SignInButtonClicked) })
+        }
     }
 }
 
