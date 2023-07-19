26c63d67d885eb231d20a67f29eafde5245e1260
Maksim Mishenko <kirova32a@mail.ru>
2023-07-19 18:27:31 +0600

    [~] add receiver

---
+++ tabletApp/src/androidMain/kotlin/band/effective/office/tablet/AdminReceiver.kt
@@ -0,0 +1,13 @@
+package band.effective.office.tablet
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.ComponentName
+import android.content.Context
+
+class AdminReceiver : DeviceAdminReceiver() {
+    companion object {
+        fun getComponentName(context: Context): ComponentName {
+            return ComponentName(context.applicationContext, AdminReceiver::class.java)
+        }
+    }
+}
--- tabletApp/src/androidMain/kotlin/band/effective/office/tablet/MainActivity.kt
+++ tabletApp/src/androidMain/kotlin/band/effective/office/tablet/MainActivity.kt
@@ -1,19 +1,49 @@
 package band.effective.office.tablet
 
+import android.app.ActivityOptions
+import android.app.admin.DevicePolicyManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import band.effective.office.tablet.di.initRoomInfoKoin
 import com.arkivanov.decompose.defaultComponentContext
 import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
 
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val KIOSK_PACKAGE = "band.effective.office.tablet"
+        val APP_PACKAGES = arrayOf(KIOSK_PACKAGE)
+
+// ...
+
+        val context = this
+        val dpm = context.getSystemService(Context.DEVICE_POLICY_SERVICE)
+                as DevicePolicyManager
+        val adminName = AdminReceiver.getComponentName(context)
+        val a = dpm.isLockTaskPermitted(KIOSK_PACKAGE)
+        dpm.setLockTaskPackages(adminName, APP_PACKAGES)
+
+        // Set an option to turn on lock task mode when starting the activity.
+        val options = ActivityOptions.makeBasic()
+        options.setLockTaskEnabled(true)
+
+        // Start our kiosk app's main activity with our lock task mode option.
+        val packageManager = context.packageManager
+        val launchIntent = packageManager.getLaunchIntentForPackage(KIOSK_PACKAGE)
+        if (launchIntent != null) {
+            context.startActivity(launchIntent, options.toBundle())
+        }
+
+
         initRoomInfoKoin()
         setContent {
-            App(defaultComponentContext(),DefaultStoreFactory())
+            App(defaultComponentContext(), DefaultStoreFactory())
         }
     }
 }
