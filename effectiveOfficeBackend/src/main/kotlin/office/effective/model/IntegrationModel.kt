92e0ce798e14af8cd063d99096c2a202abd4d321
Kiselev Danil <dabikis2855@gmail.com>
2023-07-19 23:35:38 +0600

    [+] created model for Integratoions

---
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/model/IntegrationModel.kt
@@ -0,0 +1,18 @@
+package office.effective.model
+
+import java.util.*
+
+data class IntegrationModel(var name: String, var valueStr: String) {
+    var id: UUID? = null
+    var iconUrl: String? = null
+
+    constructor(
+        _id: UUID,
+        _name: String,
+        _valueStr: String,
+        _iconUrl: String
+    ) : this(_name, _valueStr) {
+        this.id = _id
+        this.iconUrl = _iconUrl
+    }
+}
