e3e0e49dbfaaf036d0553f47deba3e35739a10aa
Kiselev Danil <dabikis2855@gmail.com>
2023-07-20 13:47:17 +0600

    [+] added User repository dependency

--- effectiveOfficeBackend/src/main/kotlin/office/effective/feature/auth/di/authDIModule.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/feature/auth/di/authDIModule.kt
@@ -5,6 +5,7 @@ import office.effective.feature.auth.TokenVerifier
 import office.effective.feature.auth.converters.IntegrationModelEntityConverter
 import office.effective.feature.auth.converters.UserDTOModelConverter
 import office.effective.feature.auth.converters.UserModelEntityConverter
+import office.effective.feature.auth.repository.UserRepository
 import office.effective.feature.auth.service.IUserService
 import office.effective.feature.auth.service.UserService
 import org.koin.dsl.module
@@ -15,4 +16,5 @@ val authDIModule = module(createdAtStart = true) {
     single<IntegrationModelEntityConverter> { IntegrationModelEntityConverter() }
     single<UserModelEntityConverter> { UserModelEntityConverter() }
     single<UserDTOModelConverter> { UserDTOModelConverter() }
+    single<UserRepository> { UserRepository() }
 }
