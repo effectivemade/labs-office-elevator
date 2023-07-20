c1050d71251805ce9c22016b157202209beea43e
Daniil Zavyalov <dezavv1@gmail.com>
2023-07-20 18:46:29 +0600

    [+] add basic routing and services

---
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/workspace/facade/WorkspaceFacade.kt
@@ -0,0 +1,33 @@
+package office.effective.features.workspace.facade
+
+import io.ktor.server.plugins.*
+import office.effective.common.exception.WorkspaceNotFoundException
+import office.effective.features.workspace.converters.WorkspaceFacadeConverter
+import office.effective.features.workspace.dto.WorkspaceDTO
+import office.effective.features.workspace.service.WorkspaceService
+import office.effective.model.Workspace
+import java.lang.IllegalArgumentException
+import java.util.UUID
+
+class WorkspaceFacade {
+    private val service = WorkspaceService()
+    private val converter = WorkspaceFacadeConverter()
+
+    fun findById(id: String): WorkspaceDTO {
+        val uuid: UUID
+        try {
+            uuid = UUID.fromString(id)
+        } catch (ex: IllegalArgumentException) {
+            throw BadRequestException("Provided id is not UUID: " + ex.message)
+        }
+        val workspace: Workspace = service.findById(uuid)
+            ?: throw WorkspaceNotFoundException("Workspace with id $id not found")
+        return converter.workspaceModelToDto(workspace)
+    }
+
+    fun findAllByTag(tag: String): List<WorkspaceDTO> {
+        return service.findAllByTag(tag).map {
+            converter.workspaceModelToDto(it)
+        }
+    }
+}
