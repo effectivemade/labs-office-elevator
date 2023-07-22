c16a54bd3c65633efb0168683906df9367ddb96b
Daniil Zavyalov <dezavv1@gmail.com>
2023-07-22 20:36:17 +0600

    [~] move Authentication config to plugins

--- effectiveOfficeBackend/src/main/kotlin/office/effective/Application.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/Application.kt
@@ -1,11 +1,7 @@
 package office.effective
 
 import com.typesafe.config.ConfigFactory
-import io.ktor.client.*
-import io.ktor.client.engine.apache.*
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
 import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -26,24 +22,7 @@ fun main() {
 fun Application.module() {
     configureMigration()
     configureDI()
-    install(Authentication) {
-        oauth("auth-oauth-google") {
-            urlProvider = { "http://localhost:8080/callback" }
-            providerLookup = {
-                OAuthServerSettings.OAuth2ServerSettings(
-                    name = "google",
-                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
-                    requestMethod = HttpMethod.Get,
-                    clientId = System.getenv("GOOGLE_CLIENT_ID"),
-                    clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
-                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile", "email"),
-                    extraAuthParameters = listOf("access_type" to "offline")
-                )
-            }
-            client = HttpClient(Apache)
-        }
-    }
+    configureAuthentication()
     configureSerialization()
     configureRouting()
     configureExceptionHandling()
