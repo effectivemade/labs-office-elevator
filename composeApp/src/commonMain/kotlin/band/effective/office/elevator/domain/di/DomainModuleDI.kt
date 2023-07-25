147bc04c0c93c57f40f3bd5f5dadec83bc3a45eb
Slava <plumsoftwareofficial@gmail.com>
2023-07-25 13:34:33 +0600

    Phone repository added to domain module

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/domain/di/DomainModuleDI.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/domain/di/DomainModuleDI.kt
@@ -1,9 +1,12 @@
 package band.effective.office.elevator.domain.di
 
 import band.effective.office.elevator.data.OfficeElevatorRepositoryImpl
+import band.effective.office.elevator.data.repository.UserPhoneNumberRepositoryImpl
 import band.effective.office.elevator.domain.OfficeElevatorRepository
+import band.effective.office.elevator.domain.repository.UserPhoneNumberRepository
 import org.koin.dsl.module
 
 internal val domainModuleDI = module {
     single<OfficeElevatorRepository> { OfficeElevatorRepositoryImpl(get(), get()) }
+    single<UserPhoneNumberRepository> { UserPhoneNumberRepositoryImpl() }
 }
