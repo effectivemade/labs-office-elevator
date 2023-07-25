dbab1d0e1fb4798ec5bfa901469fa83f526920df
Kiselev Danil <dabikis2855@gmail.com>
2023-07-25 12:13:19 +0600

    [~] changed http client

--- effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/Authentication.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/Authentication.kt
@@ -2,9 +2,17 @@ package office.effective.plugins
 
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+
+val applicationHttpClient = HttpClient(Apache) {
+    install(ContentNegotiation) {
+        json()
+    }
+}
 
 fun Application.configureAuthentication() {
     install(Authentication) {
@@ -15,14 +23,14 @@ fun Application.configureAuthentication() {
                     name = "google",
                     authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
                     accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
-                    requestMethod = HttpMethod.Get,
+                    requestMethod = HttpMethod.Post,
                     clientId = System.getenv("GOOGLE_CLIENT_ID"),
                     clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
                     defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile", "email"),
                     extraAuthParameters = listOf("access_type" to "offline")
                 )
             }
-            client = HttpClient(Apache)
+            client = applicationHttpClient
         }
     }
 }
