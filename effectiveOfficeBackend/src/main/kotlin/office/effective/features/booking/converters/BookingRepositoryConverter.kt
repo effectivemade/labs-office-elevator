d64b43d2d7fd5056eabcbebf74895859ac462b23
Daniil Zavyalov <dezavv1@gmail.com>
2023-07-31 18:52:09 +0600

    [+] converter modelToEntity method

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/booking/converters/BookingRepositoryConverter.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/booking/converters/BookingRepositoryConverter.kt
@@ -2,18 +2,18 @@ package office.effective.features.booking.converters
 
 import office.effective.features.booking.repository.WorkspaceBookingEntity
 import office.effective.features.user.converters.UserModelEntityConverter
-import office.effective.features.user.repository.IntegrationEntity
 import office.effective.features.user.repository.UserEntity
+import office.effective.features.user.repository.users
 import office.effective.features.workspace.converters.WorkspaceRepositoryConverter
 import office.effective.model.Booking
-import java.util.UUID
+import org.ktorm.database.Database
 
 class BookingRepositoryConverter(private val workspaceConverter: WorkspaceRepositoryConverter,
                                  private val userConverter: UserModelEntityConverter) {
 
     /**
      * Converts booking entity to model which contains user and workspace models.
-     * User models contain an empty list of integrations. Wo utilities
+     * User/workspace models contain an empty list of integrations/utilities.
      */
     fun entityToModel(bookingEntity: WorkspaceBookingEntity,
                       participants: List<UserEntity>): Booking {
@@ -28,5 +28,18 @@ class BookingRepositoryConverter(private val workspaceConverter: WorkspaceReposi
             bookingEntity.beginBooking,
             bookingEntity.endBooking
         )
+    }
+
+    /**
+     * Converts booking model to entity
+     */
+    fun modelToEntity(bookingModel: Booking): WorkspaceBookingEntity {
+        return WorkspaceBookingEntity {
+            owner = userConverter.modelToEntity(bookingModel.owner)
+            workspace = workspaceConverter.modelToEntity(bookingModel.workspace)
+            id = bookingModel.id
+            beginBooking = bookingModel.beginBooking
+            endBooking = bookingModel.endBooking
+        }
     }
 }
