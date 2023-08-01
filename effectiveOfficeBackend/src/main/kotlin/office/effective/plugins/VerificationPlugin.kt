5aa7a88efea77ce62cbc6a9835e9f7b512c7151e
Kiselev Danil <dabikis2855@gmail.com>
2023-08-01 15:28:09 +0600

    [+] add verification plugin

---
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/VerificationPlugin.kt
@@ -0,0 +1,30 @@
+package office.effective.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.plugins.*
+import io.ktor.server.request.*
+import office.effective.features.user.TokenVerifier
+import org.koin.core.context.GlobalContext
+import io.ktor.server.application.hooks.CallSetup
+import io.ktor.server.response.*
+import office.effective.features.user.ITokenVerifier
+
+
+val VerificationPlugin = createApplicationPlugin(name = "VerificationPlugin") {
+    val verifier: ITokenVerifier = TokenVerifier()
+    println("==========================[ verification plugin installed ]==========================")
+
+    onCall {
+        run {
+            try {
+                val token = it.request.header("id_token") ?: it.response.status(HttpStatusCode.Forbidden)
+                val email = verifier.isCorrectToken(token as String)
+            } catch (ex: Exception) {
+                it.respond("verification error. \nCause by $ex")
+            }
+        }
+
+    }
+}
