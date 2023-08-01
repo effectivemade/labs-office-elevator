c72e86d920f01aa045046691f0aee520456cd028
Slava <plumsoftwareofficial@gmail.com>
2023-08-01 16:10:35 +0600

    Refactoring bottom tab [~] refactoring code to fit design

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/components/TabNavigationItem.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/components/TabNavigationItem.kt
@@ -1,13 +1,24 @@
 package band.effective.office.elevator.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import band.effective.office.elevator.lightGray
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import band.effective.office.elevator.navigation.Tab
 import band.effective.office.elevator.textGrayColor
 import dev.icerock.moko.resources.compose.stringResource
@@ -21,12 +32,31 @@ internal fun RowScope.TabNavigationItem(
     val selectedColor = MaterialTheme.colors.primary
     val unselectedColor = textGrayColor
     BottomNavigationItem(
-        modifier = Modifier,
+        modifier = Modifier.fillMaxWidth().wrapContentHeight(),
         selected = selected,
         selectedContentColor = selectedColor,
         unselectedContentColor = unselectedColor,
         onClick = onSelect,
-        icon = { Icon(imageVector = tab.icon, contentDescription = stringResource(tab.title)) },
-        label = { Text(stringResource (tab.title), color = if (selected) selectedColor else unselectedColor) }
+        icon = {
+            Column(
+                modifier = Modifier,
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(height = 8.dp))
+                Icon(
+                    modifier = Modifier.size(size = 24.dp),
+                    imageVector = tab.icon,
+                    contentDescription = stringResource(tab.title)
+                )
+                Spacer(modifier = Modifier.height(height = 4.dp))
+                Text(
+                    text = stringResource(tab.title),
+                    style = MaterialTheme.typography.caption.copy(
+                        color = if (selected) selectedColor else unselectedColor
+                    )
+                )
+            }
+        }
     )
 }
