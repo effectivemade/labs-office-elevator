23df758df88fd3f35eb1081e9b52ec38d4bb5909
Kiselev Danil <dabikis2855@gmail.com>
2023-08-01 23:28:31 +0600

    [+] add option to turn verification plugin off

--- effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/VerificationPlugin.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/VerificationPlugin.kt
@@ -14,15 +14,19 @@ import office.effective.features.user.ITokenVerifier
 
 val VerificationPlugin = createApplicationPlugin(name = "VerificationPlugin") {
     val verifier: ITokenVerifier = TokenVerifier()
+    val pluginOn: Boolean = System.getenv("VERIFICATION_PLUGIN_ENABLE").equals("true")
+    println("Verification plugin mode enabled?: $pluginOn")
     println("==========================[ verification plugin installed ]==========================")
+    if (pluginOn) {
+        onCall {
+            run {
 
-    onCall {
-        run {
-            try {
-                val token = it.request.header("id_token") ?: it.response.status(HttpStatusCode.Forbidden)
-                val email = verifier.isCorrectToken(token as String)
-            } catch (ex: Exception) {
-                it.respond("verification error. \nCause by $ex")
+                try {
+                    val token = it.request.header("id_token") ?: it.response.status(HttpStatusCode.Forbidden)
+                    val email = verifier.isCorrectToken(token as String)
+                } catch (ex: Exception) {
+                    it.respond("verification error. \nCause by $ex")
+                }
             }
         }
 
