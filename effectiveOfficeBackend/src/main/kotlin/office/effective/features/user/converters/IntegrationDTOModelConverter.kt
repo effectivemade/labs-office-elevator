e3d61ecf003e8d32e391364ddcf66aaf4a7fe355
Kiselev Danil <dabikis2855@gmail.com>
2023-08-03 13:47:57 +0600

    [+] add id to IntegrationDTO

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/user/converters/IntegrationDTOModelConverter.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/user/converters/IntegrationDTOModelConverter.kt
@@ -2,13 +2,14 @@ package office.effective.features.user.converters
 
 import office.effective.features.user.dto.IntegrationDTO
 import office.effective.model.IntegrationModel
+import java.util.*
 
 class IntegrationDTOModelConverter {
     fun dTOToModel(integrationDTO: IntegrationDTO): IntegrationModel {
-        return IntegrationModel(null, integrationDTO.name, integrationDTO.value, "")
+        return IntegrationModel(UUID.fromString(integrationDTO.id), integrationDTO.name, integrationDTO.value, "")
     }
 
     fun modelToDTO(integrationModel: IntegrationModel): IntegrationDTO {
-        return IntegrationDTO(integrationModel.name, integrationModel.valueStr)
+        return IntegrationDTO(integrationModel.id.toString(), integrationModel.name, integrationModel.valueStr)
     }
 }
