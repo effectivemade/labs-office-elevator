3a37e7ea6f8d087e1ad0045e56ecb6b70d405bd2
Kiselev Danil <dabikis2855@gmail.com>
2023-08-06 15:25:02 +0600

    [+] connect swagger calls

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/user/routes/AuthRouting.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/user/routes/AuthRouting.kt
@@ -1,26 +1,14 @@
 import io.github.smiley4.ktorswaggerui.dsl.get
-import io.github.smiley4.ktorswaggerui.dsl.put
-import io.github.smiley4.ktorswaggerui.dsl.route
-import io.ktor.client.request.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import office.effective.common.swagger.SwaggerDocument
 import office.effective.common.utils.DatabaseTransactionManager
 
 import office.effective.features.user.ITokenVerifier
-import office.effective.features.user.converters.UserDTOModelConverter
-import office.effective.features.user.dto.UserDTO
-import office.effective.features.user.repository.UserRepository
-import office.effective.features.user.routes.swagger.alterUser
-import office.effective.features.user.routes.swagger.returnUserByEmail
-import office.effective.features.user.routes.swagger.returnUserById
-import office.effective.features.user.routes.swagger.returnUsers
-import office.effective.features.user.service.IUserService
-import office.effective.features.workspace.repository.WorkspaceTags
+import office.effective.features.user.routes.swagger.callback
+import office.effective.features.user.routes.swagger.login
 import org.koin.core.context.GlobalContext
 
 fun Route.authRoutingFun() {
@@ -28,10 +16,10 @@ fun Route.authRoutingFun() {
     val transactionManager: DatabaseTransactionManager = GlobalContext.get().get()
 
     authenticate("auth-oauth-google") {
-        get("/login") {
+        get("/login", SwaggerDocument.login()) {
 
         }
-        get("/callback") {
+        get("/callback", SwaggerDocument.callback()) {
             try {
                 val principal: OAuthAccessTokenResponse.OAuth2 =
                     call.principal() ?: throw Exception("Token cannot be verified")
