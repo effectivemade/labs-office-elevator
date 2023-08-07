f63b1522b65c04e48d4cb024f313f8fc49f5fdb7
Slava <plumsoftwareofficial@gmail.com>
2023-08-07 14:27:05 +0600

    SQL requests [+] refactoring SQL table [+] created functions to insert and get data from SQL table

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/data/database/DBSource.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/data/database/DBSource.kt
@@ -1,8 +1,37 @@
 package band.effective.office.elevator.data.database
 
-import band.effective.office.elevator.ui.content.ContentComponent
+import band.effective.office.elevator.domain.models.User
 
-// TODO(edit this interface)
 interface DBSource {
-    fun getProfileName(): String
+    fun getIdToken(): String
+
+    fun getName(): String
+
+    fun getPost(): String
+
+    fun getEmail(): String
+
+    fun getPhoneNumber(): String
+
+    fun getTelegramNick(): String
+
+    fun getImageUrl(): String?
+
+    fun getUser(): User
+
+    fun updateToken(idToken: String)
+
+    fun updateName(name: String, idToken: String)
+
+    fun updatePost(post: String, idToken: String)
+
+    fun updateEmail(email: String, idToken: String)
+
+    fun updatePhoneNumber(phoneNumber: String, idToken: String)
+
+    fun updateTelegramNick(telegramNick: String, idToken: String)
+
+    fun updateImageUrl(imageUrl: String, idToken: String)
+
+    fun insertUser(user: User)
 }
--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/data/database/DBSourceImpl.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/data/database/DBSourceImpl.kt
@@ -1,14 +1,80 @@
 package band.effective.office.elevator.data.database
 
 import band.effective.office.elevator.Database
+import band.effective.office.elevator.domain.models.User
 
 class DBSourceImpl(
     val database: Database
-): DBSource {
+) : DBSource {
 
     private val profileQueries = database.profileQueries
 
-    override fun getProfileName(): String {
-        return profileQueries.selectAll().executeAsOne().full_name
+    override fun getIdToken(): String = profileQueries.selectIdToken().executeAsOne()
+
+    override fun getName(): String = profileQueries.selectName().executeAsOne()
+
+    override fun getPost(): String = profileQueries.selectPost().executeAsOne()
+
+    override fun getEmail(): String = profileQueries.selectEmail().executeAsOne()
+
+    override fun getPhoneNumber(): String = profileQueries.selectPhoneNumber().executeAsOne()
+
+    override fun getTelegramNick(): String = profileQueries.selectTelegramNick().executeAsOne()
+
+    override fun getImageUrl(): String = profileQueries.selectImageUrl().executeAsOne()
+
+    override fun getUser(): User =
+        with(profileQueries.selectAll().executeAsOne()) {
+            User(
+                id = idToken,
+                phoneNumber = phoneNumber,
+                post = post,
+                userName = name,
+                telegram = telegramNick,
+                email = email,
+                imageUrl = imageUrl
+            )
+        }
+
+    override fun updateToken(idToken: String) {
+        profileQueries.updateToken(idToken = idToken, idToken_ = idToken)
+    }
+
+    override fun updateName(name: String, idToken: String) {
+        profileQueries.updateName(name = name, idToken = idToken)
+    }
+
+    override fun updatePost(post: String, idToken: String) {
+        profileQueries.updatePost(post = post, idToken = idToken)
+    }
+
+    override fun updateEmail(email: String, idToken: String) {
+        profileQueries.updateEmail(email = email, idToken = idToken)
+    }
+
+    override fun updatePhoneNumber(phoneNumber: String, idToken: String) {
+        profileQueries.updatePhoneNumber(phoneNumber = phoneNumber, idToken = idToken)
+    }
+
+    override fun updateTelegramNick(telegramNick: String, idToken: String) {
+        profileQueries.updateTelegramNick(telegramNick = telegramNick, idToken = idToken)
+    }
+
+    override fun updateImageUrl(imageUrl: String, idToken: String) {
+        profileQueries.updateImageUrl(imageUrl = imageUrl, idToken = idToken)
+    }
+
+    override fun insertUser(user: User) {
+        with(user) {
+            profileQueries.insert(
+                idToken = id,
+                name = userName,
+                post = post,
+                email = email,
+                phoneNumber = phoneNumber,
+                telegramNick = telegram,
+                imageUrl = imageUrl
+            )
+        }
     }
 }
