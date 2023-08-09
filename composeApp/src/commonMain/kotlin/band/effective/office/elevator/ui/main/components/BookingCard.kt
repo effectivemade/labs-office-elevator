687bde6bbda89ab62ac0264b7ab02bae3279116c
Slava <plumsoftwareofficial@gmail.com>
2023-08-09 23:00:38 +0600

    Booking card [~] refactoring code

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/main/components/BookingCard.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/main/components/BookingCard.kt
@@ -10,13 +10,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
+import androidx.compose.material3.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,6 +32,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import band.effective.office.elevator.MainRes
+import band.effective.office.elevator.textGrayColor
 import band.effective.office.elevator.ui.models.ReservedSeat
 import dev.icerock.moko.resources.compose.painterResource
 import dev.icerock.moko.resources.compose.stringResource
@@ -36,58 +43,32 @@ fun BookingCard(
     onClickShowMap: () -> Unit,
     onClickShowOptions: () -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .clip(RoundedCornerShape(16.dp))
-            .background(Color.White)
-            .padding(horizontal = 16.dp, vertical = 24.dp)
-            .fillMaxWidth()
-    ) {
-        Row {
-            SeatIcon()
-            Spacer(modifier = Modifier.width(12.dp))
-            SeatTitle(seat)
-        }
-        Spacer(modifier = Modifier.height(24.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
+    Row() {
+        Column(
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.White)
+                .padding(horizontal = 16.dp, vertical = 24.dp)
+                .fillMaxWidth()
         ) {
-            Button(
-                onClick = onClickShowMap,
-                modifier = Modifier
-                    .clip(RoundedCornerShape(8.dp))
-                    .fillMaxWidth(0.7f),
-                border = BorderStroke(1.dp, MaterialTheme.colors.primary),
-                elevation = ButtonDefaults.elevation(0.dp, 0.dp, 0.dp, 0.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.White,
-                    contentColor = MaterialTheme.colors.primary
-                )
-            ) {
-                Text(
-                    text = stringResource(MainRes.strings.show_map),
-                    fontSize = 15.sp,
-                    style = MaterialTheme.typography.button,
-                    modifier = Modifier.padding(horizontal = 8.dp)
-                )
-            }
-            Spacer(modifier = Modifier.width(16.dp))
-            Button(
-                onClick = onClickShowOptions,
-                modifier = Modifier
-                    .clip(RoundedCornerShape(8.dp)),
-                border = BorderStroke(1.dp, MaterialTheme.colors.primary),
-                elevation = ButtonDefaults.elevation(0.dp, 0.dp, 0.dp, 0.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.White,
-                    contentColor = MaterialTheme.colors.primary
-                )
-            ) {
-                Image(
-                    painter = painterResource(MainRes.images.mi_options_vertical),
-                    contentDescription = null,
-                )
+            Row {
+                SeatIcon()
+                Spacer(modifier = Modifier.width(12.dp))
+                SeatTitle(seat)
+                Spacer(modifier = Modifier.height(24.dp))
+                IconButton(
+                    onClick = onClickShowOptions,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(80.dp)),
+                    colors = IconButtonDefaults.iconButtonColors(),
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.MoreVert,
+                        contentDescription = "show option menu",
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        tint = MaterialTheme.colors.secondaryVariant
+                    )
+                }
             }
         }
     }
@@ -98,8 +79,7 @@ private fun SeatTitle(seat: ReservedSeat) {
     Column {
         Text(
             text = seat.seatName,
-            fontSize = 15.sp,
-            color = Color.Black
+            style = MaterialTheme.typography.subtitle1.copy(color = Color.Black)
         )
         Spacer(modifier = Modifier.height(12.dp))
         Row(
@@ -107,19 +87,17 @@ private fun SeatTitle(seat: ReservedSeat) {
         ) {
             Text(
                 text = seat.bookingDay,
-                fontSize = 15.sp,
-                color = Color.Black
+                style = MaterialTheme.typography.subtitle1.copy(color = textGrayColor)
             )
             Divider(
                 modifier = Modifier
-                    .width(19.dp)
+                    .width(6.dp)
                     .rotate(90f),
                 thickness = 2.dp,
             )
             Text(
                 text = seat.bookingTime,
-                fontSize = 15.sp,
-                color = Color.Black
+                style = MaterialTheme.typography.subtitle1.copy(color = textGrayColor)
             )
         }
     }
