c2b18abd40276657cfde0abb02c271a5ff2db7e8
Slava <plumsoftwareofficial@gmail.com>
2023-08-10 14:44:32 +0600

    SQL [~] refactoring code

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/data/database/DBSource.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/data/database/DBSource.kt
@@ -3,39 +3,10 @@ package band.effective.office.elevator.data.database
 import band.effective.office.elevator.scheme.ProfileData
 
 interface DBSource {
-    fun getIdToken(idToken: String): String
-
-    fun getName(idToken: String): String
-
-    fun getPost(idToken: String): String
-
-    fun getEmail(idToken: String): String
-
-    fun getPhoneNumber(idToken: String): String
-
-    fun getTelegramNick(idToken: String): String
-
-    fun getImageUrl(idToken: String): String?
 
     fun getUser(idToken: String): ProfileData
 
     fun getAll(): List<ProfileData>
-
-    fun updateToken(idToken: String)
-
-    fun updateName(name: String, idToken: String)
-
-    fun updatePost(post: String, idToken: String)
-
-    fun updateEmail(email: String, idToken: String)
-
-    fun updatePhoneNumber(phoneNumber: String, idToken: String)
-
-    fun updateTelegramNick(telegramNick: String, idToken: String)
-
-    fun updateImageUrl(imageUrl: String, idToken: String)
-
-    fun insertUser(profileData: ProfileData)
 
     fun update(profileData: ProfileData)
 }
--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/data/database/DBSourceImpl.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/data/database/DBSourceImpl.kt
@@ -10,27 +10,6 @@ class DBSourceImpl(
 
     private val profileQueries = database.profileQueries
 
-    override fun getIdToken(idToken: String): String =
-        profileQueries.selectIdToken(idToken = idToken).executeAsOne()
-
-    override fun getName(idToken: String): String =
-        profileQueries.selectName(idToken = idToken).executeAsOne()
-
-    override fun getPost(idToken: String): String =
-        profileQueries.selectPost(idToken = idToken).executeAsOne()
-
-    override fun getEmail(idToken: String): String =
-        profileQueries.selectEmail(idToken = idToken).executeAsOne()
-
-    override fun getPhoneNumber(idToken: String): String =
-        profileQueries.selectPhoneNumber(idToken = idToken).executeAsOne()
-
-    override fun getTelegramNick(idToken: String): String =
-        profileQueries.selectTelegramNick(idToken = idToken).executeAsOne()
-
-    override fun getImageUrl(idToken: String): String =
-        profileQueries.selectImageUrl(idToken = idToken).executeAsOne()
-
     override fun getUser(idToken: String): ProfileData =
         with(profileQueries.selectUser(idToken = idToken).executeAsOne()) {
             ProfileData(
@@ -45,48 +24,6 @@ class DBSourceImpl(
         }
 
     override fun getAll(): List<ProfileData> = profileQueries.selectAll().executeAsList()
-
-    override fun updateToken(idToken: String) {
-        profileQueries.updateToken(idToken = idToken, idToken_ = idToken)
-    }
-
-    override fun updateName(name: String, idToken: String) {
-        profileQueries.updateName(name = name, idToken = idToken)
-    }
-
-    override fun updatePost(post: String, idToken: String) {
-        profileQueries.updatePost(post = post, idToken = idToken)
-    }
-
-    override fun updateEmail(email: String, idToken: String) {
-        profileQueries.updateEmail(email = email, idToken = idToken)
-    }
-
-    override fun updatePhoneNumber(phoneNumber: String, idToken: String) {
-        profileQueries.updatePhoneNumber(phoneNumber = phoneNumber, idToken = idToken)
-    }
-
-    override fun updateTelegramNick(telegramNick: String, idToken: String) {
-        profileQueries.updateTelegramNick(telegramNick = telegramNick, idToken = idToken)
-    }
-
-    override fun updateImageUrl(imageUrl: String, idToken: String) {
-        profileQueries.updateImageUrl(imageUrl = imageUrl, idToken = idToken)
-    }
-
-    override fun insertUser(profileData: ProfileData) {
-        with(profileData) {
-            profileQueries.insert(
-                idToken = idToken,
-                name = name,
-                post = post,
-                email = email,
-                phoneNumber = phoneNumber,
-                telegramNick = telegramNick,
-                imageUrl = imageUrl
-            )
-        }
-    }
 
     override fun update(profileData: ProfileData) {
         with(profileData) {
