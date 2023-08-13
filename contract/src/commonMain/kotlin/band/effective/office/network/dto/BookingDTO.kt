5bbeef4b1a0f2463fd23bd697626dc6b149160cd
Maksim Mishenko <kirova32a@mail.ru>
2023-08-13 21:24:32 +0600

    [~] fix BookingDTO.kt

--- contract/src/commonMain/kotlin/band/effective/office/network/dto/BookingDTO.kt
+++ contract/src/commonMain/kotlin/band/effective/office/network/dto/BookingDTO.kt
@@ -4,10 +4,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class BookingDTO (
-    var owner: UserDTO,
-    var participants: List<UserDTO>,
-    var workspace: WorkspaceDTO,
-    var id: String?,
-    var beginBooking: Long,
-    var endBooking: Long
+    val owner: UserDTO,
+    val participants: List<UserDTO>,
+    val workspace: WorkspaceDTO,
+    val id: String?,
+    val beginBooking: Long,
+    val endBooking: Long
 )
