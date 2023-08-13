224e29f58c3ec9bd033a76866539bab484677cf2
Daniil Zavyalov <dezavv1@gmail.com>
2023-08-13 17:10:46 +0600

    [~] add MIGRATIONS_ENABLE env variable

--- effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/Migration.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/Migration.kt
@@ -21,16 +21,19 @@ fun Application.configureMigration() {
         ?.getString() ?: "changelog/changelog-master.yaml"
     val defaultSchemaName: String = config.propertyOrNull("liquibase.defaultSchemaName")
         ?.getString() ?: "public"
+    val migrationsEnable: Boolean = System.getenv("MIGRATIONS_ENABLE").equals("true")
 
-    val connection = DriverManager.getConnection(
-        url,
-        username,
-        password
-    )
-    val databaseConnection = JdbcConnection(connection)
-    val database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(databaseConnection)
-    database.defaultSchemaName = defaultSchemaName
-    val liquibase = Liquibase(changelogFile, ClassLoaderResourceAccessor(), database)
-    liquibase.update("")
-    liquibase.database.close()
+    if (migrationsEnable) {
+        val connection = DriverManager.getConnection(
+            url,
+            username,
+            password
+        )
+        val databaseConnection = JdbcConnection(connection)
+        val database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(databaseConnection)
+        database.defaultSchemaName = defaultSchemaName
+        val liquibase = Liquibase(changelogFile, ClassLoaderResourceAccessor(), database)
+        liquibase.update("")
+        liquibase.database.close()
+    }
 }
