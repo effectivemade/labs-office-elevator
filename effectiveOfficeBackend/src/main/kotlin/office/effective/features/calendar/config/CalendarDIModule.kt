aeb85d23d199443df5a2981b2311db14bfd875e5
Kiselev Danil <dabikis2855@gmail.com>
2023-08-16 22:47:55 +0600

    [+] add calendar DI module

---
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/calendar/config/CalendarDIModule.kt
@@ -0,0 +1,8 @@
+package office.effective.features.calendar.config
+
+import office.effective.features.calendar.repository.CalendarRepository
+import org.koin.dsl.module
+
+val calendarDiModule = module(createdAtStart = true) {
+    single { CalendarRepository(get()) }
+}
--- effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/DI.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/DI.kt
@@ -3,6 +3,7 @@ package office.effective.plugins
 import io.ktor.server.application.*
 import office.effective.common.utils.commonDiModule
 import office.effective.features.booking.config.bookingDiModule
+import office.effective.features.calendar.config.calendarDiModule
 import office.effective.features.simpleAuth.di.authDiModule
 import office.effective.features.user.di.userDIModule
 import office.effective.features.workspace.config.workspaceDiModule
@@ -15,7 +16,8 @@ fun Application.configureDI() {
             userDIModule,
             workspaceDiModule,
             bookingDiModule,
-            authDiModule
+            authDiModule,
+            calendarDiModule
         )
     }
 }
