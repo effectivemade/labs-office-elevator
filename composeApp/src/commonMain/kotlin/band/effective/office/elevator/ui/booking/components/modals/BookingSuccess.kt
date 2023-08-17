a893440a0cc6134db76b2b45892e54986d687bf0
Slava <plumsoftwareofficial@gmail.com>
2023-08-17 15:52:40 +0600

    Booking: BookingSuccess.kt [~] refactoring code according design

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/booking/components/modals/BookingSuccess.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/ui/booking/components/modals/BookingSuccess.kt
@@ -3,8 +3,10 @@ package band.effective.office.elevator.ui.booking.components.modals
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -31,56 +33,59 @@ import dev.icerock.moko.resources.compose.stringResource
 @Composable
 fun BookingSuccess(onMain: () -> Unit, close: () -> Unit, modifier: Modifier) {
 
-    Column(
-        verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.Top),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .clip(shape = RoundedCornerShape(size = 16.dp))
-            .background(color = Color.White)
-            .padding(start = 16.dp, top = 24.dp, end = 16.dp, bottom = 24.dp)
-    ) {
+    Box(modifier = Modifier.background(color = ExtendedThemeColors.colors._66x).fillMaxSize()) {
         Column(
-            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Top),
+            verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.Top),
             horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .align(alignment = Alignment.Center)
+                .clip(shape = RoundedCornerShape(size = 16.dp))
+                .background(color = Color.White)
+                .padding(start = 16.dp, top = 24.dp, end = 16.dp, bottom = 24.dp)
         ) {
-            Text(
-                text = stringResource(
-                    resource = MainRes.strings.place_booked,
-                ),
-                style = MaterialTheme.typography.h6.copy(
-                    fontWeight = FontWeight(
-                        weight = 500
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Top),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = stringResource(
+                        resource = MainRes.strings.place_booked,
                     ),
-                    color = Color.Black
-                ),
-                textAlign = TextAlign.Center
-            )
+                    style = MaterialTheme.typography.h6.copy(
+                        fontWeight = FontWeight(
+                            weight = 500
+                        ),
+                        color = Color.Black
+                    ),
+                    textAlign = TextAlign.Center
+                )
 
-            Text(
-                text = stringResource(
-                    resource = MainRes.strings.good_working_day,
-                ),
-                style = MaterialTheme.typography.body1.copy(
-                    color = ExtendedThemeColors.colors._66x
-                ),
-                textAlign = TextAlign.Center
-            )
-        }
+                Text(
+                    text = stringResource(
+                        resource = MainRes.strings.good_working_day,
+                    ),
+                    style = MaterialTheme.typography.body1.copy(
+                        color = ExtendedThemeColors.colors._66x
+                    ),
+                    textAlign = TextAlign.Center
+                )
+            }
 
-        Column(
-            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Top),
-            horizontalAlignment = Alignment.Start,
-        ) {
-            EffectiveButton(
-                buttonText = stringResource(resource = MainRes.strings.move_to_main_from_booking),
-                onClick = onMain
-            )
-            EffectiveButton(
-                buttonText = stringResource(resource = MainRes.strings.close_booking),
-                onClick = close
-            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Top),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                EffectiveButton(
+                    buttonText = stringResource(resource = MainRes.strings.move_to_main_from_booking),
+                    onClick = onMain
+                )
+                EffectiveButton(
+                    buttonText = stringResource(resource = MainRes.strings.close_booking),
+                    onClick = close
+                )
+            }
         }
     }
 }
