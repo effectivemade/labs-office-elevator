5c0a407d536406eb559d32fb5c6dca6723d7ef99
Kiselev Danil <dabikis2855@gmail.com>
2023-08-17 23:43:56 +0600

    [~] change CalendarRepository constructor paramethrs number

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/calendar/config/CalendarDIModule.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/calendar/config/CalendarDIModule.kt
@@ -4,5 +4,5 @@ import office.effective.features.calendar.repository.CalendarRepository
 import org.koin.dsl.module
 
 val calendarDiModule = module(createdAtStart = true) {
-    single { CalendarRepository(get()) }
+    single { CalendarRepository(get(), get(), get()) }
 }
