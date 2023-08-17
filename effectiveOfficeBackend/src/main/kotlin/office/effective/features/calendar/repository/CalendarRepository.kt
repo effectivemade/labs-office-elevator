78085e05d85f7acc49a8bdbc2e7f515d180de3fe
Kiselev Danil <dabikis2855@gmail.com>
2023-08-17 23:43:24 +0600

    [+] add findWorkspaceById method

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/calendar/repository/CalendarRepository.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/calendar/repository/CalendarRepository.kt
@@ -1,15 +1,34 @@
 package office.effective.features.calendar.repository
 
 import office.effective.common.exception.InstanceNotFoundException
+import office.effective.features.workspace.converters.WorkspaceRepositoryConverter
+import office.effective.features.workspace.repository.WorkspaceEntity
+import office.effective.features.workspace.repository.WorkspaceRepository
+import office.effective.model.Workspace
+import org.koin.core.context.GlobalContext
 import org.ktorm.database.Database
 import org.ktorm.dsl.eq
 import org.ktorm.entity.find
 import java.util.UUID
 
-class CalendarRepository(private val db: Database) {
+class CalendarRepository(
+    private val db: Database,
+    private val converter: WorkspaceRepositoryConverter,
+    private val workspaceRepository: WorkspaceRepository
+) {
     fun findByWorkspace(workspaceId: UUID): String {
         return db.calendarIds.find { it.workspaceId eq workspaceId }?.calendarId ?: throw InstanceNotFoundException(
             CalendarIdEntity::class, "Cannot found calendar id to workspace id $workspaceId", null
+        )
+    }
+
+    fun findWorkspaceById(calendarId: String): Workspace {
+        val workspaceEntity =
+            db.calendarIds.find { it.calendarId eq calendarId }?.workspace ?: throw InstanceNotFoundException(
+                WorkspaceEntity::class, "Workspace with such google calendar id not found", null
+            )
+        return converter.entityToModel(
+            workspaceEntity, workspaceRepository.findUtilitiesByWorkspaceId(workspaceEntity.id)
         )
     }
 }
