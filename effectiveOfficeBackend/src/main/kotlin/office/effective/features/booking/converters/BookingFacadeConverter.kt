750daa0c919a7d25ea6c388dd45eab82e5cc8021
Kiselev Danil <dabikis2855@gmail.com>
2023-08-19 18:19:35 +0600

    [~] add recurrence into constructors

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/booking/converters/BookingFacadeConverter.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/booking/converters/BookingFacadeConverter.kt
@@ -1,10 +1,13 @@
 package office.effective.features.booking.converters
 
+import model.RecurrenceDTO
 import office.effective.common.utils.UuidValidator
 import office.effective.dto.BookingDTO
+import office.effective.features.calendar.converters.RecurrenceConverter
 import office.effective.features.user.converters.UserDTOModelConverter
 import office.effective.features.workspace.converters.WorkspaceFacadeConverter
 import office.effective.model.Booking
+import office.effective.model.RecurrenceModel
 import java.time.Instant
 
 class BookingFacadeConverter(private val userConverter: UserDTOModelConverter,
@@ -16,13 +19,18 @@ class BookingFacadeConverter(private val userConverter: UserDTOModelConverter,
      * @author Daniil Zavyalov
      */
     fun modelToDto(booking: Booking): BookingDTO {
+        var recurrenceDTO : RecurrenceDTO? = null
+        if(booking.recurrence != null) {
+            recurrenceDTO = RecurrenceConverter.modelToDto(booking.recurrence!!)
+        }
         return BookingDTO(
             owner = userConverter.modelToDTO(booking.owner),
             participants = booking.participants.map { userConverter.modelToDTO(it) },
             workspace = workspaceConverter.modelToDto(booking.workspace),
             id = booking.id.toString(),
             beginBooking = booking.beginBooking.toEpochMilli(),
-            endBooking = booking.endBooking.toEpochMilli()
+            endBooking = booking.endBooking.toEpochMilli(),
+            recurrence = recurrenceDTO
         )
     }
 
@@ -32,13 +40,18 @@ class BookingFacadeConverter(private val userConverter: UserDTOModelConverter,
      * @author Daniil Zavyalov
      */
     fun dtoToModel(bookingDTO: BookingDTO): Booking {
+        var recurrenceModel : RecurrenceModel? = null
+        if(bookingDTO.recurrence != null) {
+            recurrenceModel = RecurrenceConverter.dtoToModel(bookingDTO.recurrence!!)
+        }
         return Booking(
             owner = userConverter.dTOToModel(bookingDTO.owner),
             participants = bookingDTO.participants.map { userConverter.dTOToModel(it) },
             workspace = workspaceConverter.dtoToModel(bookingDTO.workspace),
             id = bookingDTO.id?.let { uuidValidator.uuidFromString(it) },
             beginBooking = Instant.ofEpochMilli(bookingDTO.beginBooking),
-            endBooking = Instant.ofEpochMilli(bookingDTO.endBooking)
+            endBooking = Instant.ofEpochMilli(bookingDTO.endBooking),
+            recurrence = recurrenceModel
         )
     }
 }
