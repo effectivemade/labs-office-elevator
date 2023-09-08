2aee35bfe4224033660a9915e0d9b66a91f382e4
Kiselev Danil <dabikis2855@gmail.com>
2023-09-08 17:01:38 +0600

    [~] optimise date parcing

--- effectiveOfficeBackend/src/main/kotlin/office/effective/model/Recurrence.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/model/Recurrence.kt
@@ -26,12 +26,12 @@ data class Recurrence(
 ) {
     companion object {
         fun RecurrenceDTO.toRecurrence(): Recurrence =
-            Recurrence(interval = (interval ?: 0).apply { if (this < 0) throw IllegalArgumentException() },
+            Recurrence(interval = (interval ?: 0).apply { if (this < 0) throw IllegalArgumentException("Interval must be greater then 0") },
                 freq = Freq.valueOf(freq),
                 ending = when {
                     count != null && until != null -> throw IllegalArgumentException()
                     count != null -> Ending.Count(count)
-                    until != null -> Ending.Until(toDateRfc5545(until))//(until.toString())
+                    until != null -> Ending.Until(toDateRfc5545(until))
 
                     else -> Ending.Empty
                 },
@@ -70,44 +70,13 @@ data class Recurrence(
     private fun parceUntil(untilStr: String): Long {
         //2023 10 15 T 20 00 00 Z
         //0123 45 67 8 90 12 34 5
-        val sb = StringBuilder()
-        val str = untilStr.toCharArray()
 
-        //get year
-        for (i in 0..3) {
-            sb.append(untilStr.toCharArray()[i])
-        }
-        val year: Int = sb.toString().toInt()
-
-        // get month
-        sb.clear()
-        sb.append(str[4])
-        sb.append(str[5])
-        val month: Int = sb.toString().toInt()
-
-        // get day
-        sb.clear()
-        sb.append(str[6])
-        sb.append(str[7])
-        val day: Int = sb.toString().toInt()
-
-        // get hour
-        sb.clear()
-        sb.append(str[9])
-        sb.append(str[10])
-        val hour: Int = sb.toString().toInt()
-
-        // get min
-        sb.clear()
-        sb.append(str[11])
-        sb.append(str[12])
-        val min: Int = sb.toString().toInt()
-
-        // get sec
-        sb.clear()
-        sb.append(str[13])
-        sb.append(str[14])
-        val sec: Int = sb.toString().toInt()
+        val year: Int = untilStr.substring(0,4).toInt()//sb.toString().toInt()
+        val month: Int = untilStr.substring(4,6).toInt()
+        val day: Int = untilStr.substring(6,8).toInt()
+        val hour: Int = untilStr.substring(9,11).toInt()
+        val min: Int = untilStr.substring(11,13).toInt()
+        val sec: Int = untilStr.substring(13,15).toInt()
 
         val dt = Date(year - 1900, month, day, hour, min, sec)
         return dt.time
