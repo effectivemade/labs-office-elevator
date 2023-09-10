4db4d8c450c951a69a595fcb669161bccb3e0f7b
Artem Gruzdev <gruzdev.artem.03@mail.ru>
2023-09-10 15:57:49 +0600

    [+] add zooming map

---
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/components/ZoombleBox.kt
@@ -0,0 +1,59 @@
+package band.effective.office.elevator.components
+
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.IntSize
+
+@Composable
+fun ZoomableBox(
+    modifier: Modifier = Modifier,
+    minScale: Float = 0.1f,
+    maxScale: Float = 5f,
+    content: @Composable ZoomableBoxScope.() -> Unit
+) {
+    var scale by remember { mutableStateOf(1f) }
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+    var size by remember { mutableStateOf(IntSize.Zero) }
+    Box(
+        modifier = modifier
+            .clip(RectangleShape)
+            .onSizeChanged { size = it }
+            .pointerInput(Unit) {
+                detectTransformGestures { _, pan, zoom, _ ->
+                    scale = maxOf(minScale, minOf(scale * zoom, maxScale))
+                    val maxX = (size.width * (scale - 1)) / 2
+                    val minX = -maxX
+                    offsetX = maxOf(minX, minOf(maxX, offsetX + pan.x))
+                    val maxY = (size.height * (scale - 1)) / 2
+                    val minY = -maxY
+                    offsetY = maxOf(minY, minOf(maxY, offsetY + pan.y))
+                }
+            }
+    ) {
+        val scope = ZoomableBoxScopeImpl(scale, offsetX, offsetY)
+        scope.content()
+    }
+}
+
+interface ZoomableBoxScope {
+    val scale: Float
+    val offsetX: Float
+    val offsetY: Float
+}
+
+private data class ZoomableBoxScopeImpl(
+    override val scale: Float,
+    override val offsetX: Float,
+    override val offsetY: Float
+) : ZoomableBoxScope
