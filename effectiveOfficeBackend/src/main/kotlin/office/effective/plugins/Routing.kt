672dc8ad49edea22f7259ae671570459a476e4f1
Kiselev Danil <dabikis2855@gmail.com>
2023-10-18 16:30:59 +0600

    [+] add metrics routing file

--- effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/Routing.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/Routing.kt
@@ -6,6 +6,7 @@ import io.ktor.server.routing.*
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import office.effective.features.user.routes.userRouting
 import office.effective.features.booking.routes.bookingRouting
+import office.effective.features.logsAndMetriks.metrics
 import office.effective.features.notifications.routes.calendarNotificationsRouting
 import office.effective.features.workspace.routes.workspaceRouting
 import org.koin.core.context.GlobalContext
@@ -19,11 +20,8 @@ fun Application.configureRouting() {
         userRouting()
         bookingRouting()
         calendarNotificationsRouting()
-        
-        get("/metrics"){
-            val appMicrometerRegistry : PrometheusMeterRegistry = GlobalContext.get().get()
-            call.respond(appMicrometerRegistry.scrape())
-        }
+        metrics()
+
     }
 
 }
