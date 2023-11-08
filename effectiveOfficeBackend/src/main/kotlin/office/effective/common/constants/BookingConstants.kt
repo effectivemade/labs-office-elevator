08a6622ed978dfbe896e0f3cd8123a23411ba7cd
Daniil Zavyalov <dezavv1@gmail.com>
2023-11-08 21:31:35 +0600

    [~] fix booking constants

--- effectiveOfficeBackend/src/main/kotlin/office/effective/common/constants/BookingConstants.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/common/constants/BookingConstants.kt
@@ -1,5 +1,7 @@
 package office.effective.common.constants
 
+import office.effective.config
+
 /**
  * Constants for booking
  */
@@ -8,6 +10,10 @@ object BookingConstants {
      * Minimum booking start time.
      * Bookings that started earlier should be filtered out in requests.
      */
-    val MIN_SEARCH_START_TIME = office.effective.config.propertyOrNull("calendar.minTime")?.getString()?.toLong()
+    val MIN_SEARCH_START_TIME = config.propertyOrNull("calendar.minTime")?.getString()?.toLong()
         ?: throw Exception("Config file does not contain minimum time")
+    val DEFAULT_CALENDAR: String = config.propertyOrNull("calendar.defaultCalendar")?.getString()
+        ?: throw Exception("Config file does not contain Google default calendar id")
+    val WORKSPACE_CALENDAR: String = config.propertyOrNull("calendar.workspaceCalendar")?.getString()
+        ?: throw Exception("Config file does not contain workspace Google calendar id")
 }
