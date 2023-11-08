680c0f1a8374ae2e06fe70aaf90b36cdf336bb5d
Kiselev Danil <dabikis2855@gmail.com>
2023-11-08 20:55:36 +0600

    [~] add Auth pipline into DI module

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/simpleAuth/di/authDiModule.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/simpleAuth/di/authDiModule.kt
@@ -1,8 +1,25 @@
 package office.effective.features.simpleAuth.di
 
+import office.effective.features.simpleAuth.ApiKeyVerifier
+import office.effective.features.simpleAuth.ITokenVerifier
+import office.effective.features.simpleAuth.TokenVerifier
 import office.effective.features.simpleAuth.repository.AuthRepository
+import office.effective.features.simpleAuth.service.AuthenticationPipeline
+import office.effective.features.simpleAuth.service.RequestVerifier
 import org.koin.dsl.module
 
 val authDiModule = module(createdAtStart = true) {
     single { AuthRepository(get()) }
+    single { AuthenticationPipeline(makeList()) }
+}
+
+/**
+ * Describes list of authentication handlers
+ * */
+fun makeList(): List<ITokenVerifier> {
+    val list: MutableList<ITokenVerifier> = mutableListOf()
+    list.add(TokenVerifier())
+    list.add(RequestVerifier())
+    list.add(ApiKeyVerifier())
+    return list
 }
