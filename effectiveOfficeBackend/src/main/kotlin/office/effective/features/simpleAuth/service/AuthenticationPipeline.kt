4e77cae3fe357e118e183b2b79680e28a5ff8c70
Kiselev Danil <dabikis2855@gmail.com>
2023-11-14 16:51:57 +0600

    [~] add previousNode variable. Add check if verifier list is empty

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/simpleAuth/service/AuthenticationPipeline.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/simpleAuth/service/AuthenticationPipeline.kt
@@ -1,22 +1,31 @@
 package office.effective.features.simpleAuth.service
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 
 class AuthenticationPipeline(list: List<ITokenVerifier>) {
-    var firstNode: ITokenVerifier? = null
+    val logger: Logger = LoggerFactory.getLogger(this::class.java)
+    private val firstNode: ITokenVerifier
+    private var previousNode: ITokenVerifier? = null
 
     init {
         for (node in list) {
-            if (firstNode == null) {
-                firstNode = node
+            if (previousNode == null) {
+                previousNode = node
             } else {
-                firstNode?.setNext(node)
-                firstNode = node
+                previousNode?.setNext(node)
+                previousNode = node
             }
+        }
+        if (list.isEmpty()) {
+            logger.error("Empty verifier list")
+            throw RuntimeException("Empty verifier list");
         }
         firstNode = list.first()
     }
 
     suspend fun authenticateToken(token: String): Boolean {
-        return firstNode?.isCorrectToken(token) ?: false
+        return firstNode.isCorrectToken(token)
     }
 }
