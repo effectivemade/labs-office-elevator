0d196ff60f4eddc56e8bf4d86f87e2bafc830fdb
Kiselev Danil <dabikis2855@gmail.com>
2023-11-14 21:34:24 +0600

    [~] move prevNode inside init block

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/simpleAuth/service/AuthenticationPipeline.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/simpleAuth/service/AuthenticationPipeline.kt
@@ -7,14 +7,15 @@ import org.slf4j.LoggerFactory
 class AuthenticationPipeline(list: List<ITokenVerifier>) {
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
     private val firstNode: ITokenVerifier
-    private var previousNode: ITokenVerifier? = null
+
 
     init {
+        var previousNode: ITokenVerifier? = null
         for (node in list) {
             if (previousNode == null) {
                 previousNode = node
             } else {
-                previousNode?.setNext(node)
+                previousNode.setNext(node)
                 previousNode = node
             }
         }
