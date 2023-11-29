fa3a17ad85417cef6b0223bf4cd9ee6b60849db8
Kiselev Danil <dabikis2855@gmail.com>
2023-11-29 22:34:18 +0600

    [+] add an authorizer which can permit access to specified routes

---
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/simpleAuth/service/PermitAuthorizer.kt
@@ -0,0 +1,33 @@
+package office.effective.features.simpleAuth.service
+
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Authorizer to permit routes. Permit all child routes, if parent permitted.
+ * Example: "/workspaces" permission allow access to "/workspaces/zones" routes.
+ * @author Danil Kiselev
+ * */
+class PermitAuthorizer(private val permittedPaths: Iterable<String>) : ITokenAuthorizer {
+
+    val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
+    override suspend fun isCorrectToken(call: ApplicationCall): Boolean {
+        val fixedPathAsArray = call.request.path().split('/').filter { it.isNotBlank() }
+        for (permitted in permittedPaths) {
+            val permittedPathAsArray = permitted.split('/').filter { it.isNotBlank() }
+            if (permittedPathAsArray.size <= fixedPathAsArray.size) {
+                for (i in permittedPathAsArray.indices) {
+                    if (permittedPathAsArray[i] == fixedPathAsArray[i]) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+
+}
