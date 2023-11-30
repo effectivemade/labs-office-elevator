fdaf6b0933ff1346fda6a0e632d91efaa7619fd7
Kiselev Danil <dabikis2855@gmail.com>
2023-11-30 17:25:12 +0600

    [~] improve kdoc for isCorrectToken()

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/simpleAuth/service/Authorizer.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/simpleAuth/service/Authorizer.kt
@@ -3,11 +3,11 @@ package office.effective.features.simpleAuth.service
 import io.ktor.server.application.*
 
 /**
- * Interface presents methods to verify tokens
+ * Interface for authorization chain of responsibility element
  * */
 interface Authorizer {
     /**
-     * @param call [ApplicationCall] which contains token to verify
+     * @param call [ApplicationCall] incoming call to authorize
      * @author Kiselev Danil
      * */
     suspend fun isCorrectToken(call: ApplicationCall): Boolean
