d83f4843276b7d041b3ddbddd7b4a4d5964525b2
Artem Gruzdev <gruzdev.artem.03@mail.ru>
2023-12-08 21:28:18 +0600

    [~] update code version

--- tabletApp/build.gradle.kts
+++ tabletApp/build.gradle.kts
@@ -46,7 +46,6 @@ android {
         getByName("debug") {
             signingConfig = signingConfigs.getByName("debug")
             isDebuggable = true
-            applicationIdSuffix = ".debug"
         }
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
