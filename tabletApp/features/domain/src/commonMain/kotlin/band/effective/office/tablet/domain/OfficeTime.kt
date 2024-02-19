12350aaf91c44fa7910d1726738e4bbc0ed2252e
Maksim Mishenko <kirova32a@mail.ru>
2024-02-19 17:38:12 +0600

    [~] fix side effect OfficeTime.startWorkTime

--- tabletApp/features/domain/src/commonMain/kotlin/band/effective/office/tablet/domain/OfficeTime.kt
+++ tabletApp/features/domain/src/commonMain/kotlin/band/effective/office/tablet/domain/OfficeTime.kt
@@ -4,12 +4,12 @@ import java.util.Calendar
 import java.util.GregorianCalendar
 
 object OfficeTime {
-    fun startWorkTime(date: Calendar = GregorianCalendar()) = date.apply {
+    fun startWorkTime(date: Calendar = GregorianCalendar()) = (date.clone() as Calendar).apply {
         set(Calendar.HOUR_OF_DAY, 8)
         set(Calendar.MINUTE, 0)
     }
 
-    fun finishWorkTime(date: Calendar = GregorianCalendar()) = date.apply {
+    fun finishWorkTime(date: Calendar = GregorianCalendar()) = (date.clone() as Calendar).apply {
         set(Calendar.HOUR_OF_DAY, 22)
         set(Calendar.MINUTE, 0)
     }
