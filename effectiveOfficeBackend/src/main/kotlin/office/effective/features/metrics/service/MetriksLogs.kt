48d3064b8aa4871dd20b454fab88ac27e83d75e7
Kiselev Danil <dabikis2855@gmail.com>
2024-02-28 11:16:02 +0600

    [+] add metrics for office occupation calc

--- effectiveOfficeBackend/src/main/kotlin/office/effective/features/metrics/service/MetriksLogs.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/features/metrics/service/MetriksLogs.kt
@@ -1,14 +1,49 @@
 package office.effective.features.metrics.service
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import org.koin.core.context.GlobalContext
+import java.time.Instant
 
-fun Route.metrics(){
-    get("/metrics") {
-        val appMicrometerRegistry: PrometheusMeterRegistry = GlobalContext.get().get()
-        call.respond(appMicrometerRegistry.scrape())
+fun Route.metrics() {
+    route("metrics") {
+        val metricsService: MetricsService = GlobalContext.get().get()
+        get("/micrometer") {
+            val appMicrometerRegistry: PrometheusMeterRegistry = GlobalContext.get().get()
+            call.respond(appMicrometerRegistry.scrape())
+        }   
+
+        get("/percentOfFreeWorkspaces") {
+            val startTime: Instant = Instant.ofEpochMilli(call.request.queryParameters["range_from"].let {
+                it?.toLongOrNull() ?: throw BadRequestException("range_to can't be parsed to Long")
+            })
+            val endTime: Instant = Instant.ofEpochMilli(call.request.queryParameters["range_to"].let {
+                it?.toLongOrNull() ?: throw BadRequestException("range_to can't be parsed to Long")
+            })
+            call.respond(metricsService.calculateOfficeOccupancy(startTime, endTime))
+            return@get
+        }
+
+        get("/pecentOfFreeTime") {
+            val dayStarts: Int = call.request.queryParameters["day_starts"].let {
+                it?.toInt() ?: throw BadRequestException("day_starts can't be parsed to Int")
+            }
+            val dayEnds: Int =call.request.queryParameters["day_ends"].let {
+                it?.toInt() ?: throw BadRequestException("day_ends can't be parsed to Int")
+            }
+            val startTime: Instant = Instant.ofEpochMilli(call.request.queryParameters["range_from"].let {
+                it?.toLongOrNull() ?: throw BadRequestException("range_from can't be parsed to Long")
+            })
+            val endTime: Instant = Instant.ofEpochMilli(call.request.queryParameters["range_to"].let {
+                it?.toLongOrNull() ?: throw BadRequestException("range_to can't be parsed to Long")
+            })
+            call.respond(metricsService.calcPlaceHours(dayStarts, dayEnds, startTime, endTime))
+            return@get
+        }
     }
+
+
 }
--- effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/Routing.kt
+++ effectiveOfficeBackend/src/main/kotlin/office/effective/plugins/Routing.kt
@@ -3,13 +3,11 @@ package office.effective.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.micrometer.prometheus.PrometheusMeterRegistry
 import office.effective.features.user.routes.userRouting
 import office.effective.features.booking.routes.bookingRouting
-import office.effective.features.logsAndMetriks.metrics
+import office.effective.features.metrics.service.metrics
 import office.effective.features.notifications.routes.calendarNotificationsRouting
 import office.effective.features.workspace.routes.workspaceRouting
-import org.koin.core.context.GlobalContext
 
 fun Application.configureRouting() {
     routing {
